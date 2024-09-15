bf085f47061de08aec04d0e9cbe568a6ea95a6c1
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-09-14 21:29:06 -0700

    fix button

--- js/index.js
+++ js/index.js
@@ -10,15 +10,16 @@ function toggleMenu() {
 }
 
 // set year to current year for footer
-const currentYear = new Date().getFullYear();
-const copyrightYearSpan = document.getElementById("copyright-year");
+// const currentYear = new Date().getFullYear();
+// const copyrightYearSpan = document.getElementById("copyright-year");
 
-copyrightYearSpan.textContent = currentYear;
+// copyrightYearSpan.textContent = currentYear;
 
 // function to make scroll button appear
 const scrollButton = document.getElementById("scroll-up");
 
 window.addEventListener("scroll", () => {
+	console.log(window.scrollY);
 	if (window.scrollY > window.innerHeight / 5) {
 		scrollButton.classList.add("show");
 	} else {
